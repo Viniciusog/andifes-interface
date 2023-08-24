a4ca51fea89885cd4c39d5b2a0e8ec990c3341b1
viniciusog <viniciusogbr@gmail.com>
2023-08-24 17:03:46 -0300

    First

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author vinij
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        try {
+            UIManager.setLookAndFeel(new FlatIntelliJLaf());         
+        } catch (UnsupportedLookAndFeelException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        FrameLegal f = new FrameLegal();
+        f.setVisible(true);
+    }
+}
